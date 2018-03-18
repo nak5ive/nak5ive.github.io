63441e70f49d596a1cd173fda32ee43d2f6bb10c
Al Kent <al.kent@mastercard.com>
2018-03-18 16:38:08 -0500

    adding decrease buy in counts

--- poker-runner-cast/pr.js
+++ poker-runner-cast/pr.js
@@ -1,26 +1,44 @@
+// init cast framework
 const CAST_NAMESPACE = "urn:x-cast:com.nak5.pokerrunner";
-var context = cast.framework.CastReceiverContext.getInstance();
+const context = cast.framework.CastReceiverContext.getInstance();
 context.addCustomMessageListener(CAST_NAMESPACE, function(event) {
     console.log(event);
     if (event.data.action == "IncreaseBuyIn") {
         increaseBuyIn();
+    } else if (event.data.action == "DecreaseBuyIn") {
+        decreaseBuyIn();
     }
 });
 
-var options = new cast.framework.CastReceiverOptions();
+const options = new cast.framework.CastReceiverOptions();
 options.maxInactivity = 3600;
 context.start(options);
 
 // game specs
+const buyInCost = 10;
 const blindLevels = [10, 20, 40, 80, 100, 200, 400, 800, 1600, 3200];
 const blindInterval = 15 * 60 * 1000; // millis
 
 // game info
 var buyInCount = 0;
+var currentTime = 0;
 
 function increaseBuyIn() {
     $('#buyInCount').text(++buyInCount + '');
-    console.log("Buy In Count increased to " + buyInCount);
+    console.log("Buy-In Count increased to " + buyInCount);
 
-    // TODO update payouts
+    updatePayouts();
+}
+
+function decreaseBuyIn() {
+    if (buyInCount == 0) return;
+    $('#buyInCount').text(--buyInCount + '');
+    console.log("Buy-In Count decreased to " + buyInCount);
+
+    updatePayouts();
+}
+
+function updatePayouts() {
+    var total = buyInCount * buyInCost;
+    $('#payouts').text('$' + total);
 }
